1df582211d118cfb157811f48703d9ba60672439
timjinx <jinkerson@outlook.com>
2016-03-16 14:52:20 +0000

    Update flipper.pl
    Added some validation

--- Coins/perl/flipper.pl
+++ Coins/perl/flipper.pl
@@ -1,7 +1,22 @@
 use Clist;
 use Getopt::Std;
+use warnings;
+use strict;
+
+our ($opt_c,$opt_f);
 getopt('c:f:');
-$coins = Clist->new($opt_c);
+my $error=0;
+if ( ! $opt_c || $opt_c < 1 ) {
+   print "Use the -c option to specify the number of coins.\n";
+   $error=1;
+}
+if ( ! $opt_f || $opt_f < 1 ) {
+   print "Use the -f option to specify the number of flips.\n";
+   $error=1;
+}
+die "Please re-run with the correct parameters" unless $error == 0;
+
+my $coins = Clist->new($opt_c);
 my @count;
 for ( my $i=0; $i <= $opt_c; $i++) {
    $count[$i] = 0;
@@ -10,9 +25,9 @@ for ( my $i=0; $i < $opt_f; $i++) {
    $coins->flip_all();
    $count[$coins->count_heads()]++;
 }
-$totflips=0;
+my $totflips=0;
 for ( my $i=0; $i <= $opt_c; $i++) {
-   print "I threw ", $i, " heads \t", $count[$i], " times\n";
+   print "I threw ", $i, " heads \t", $count[$i], " times\n" if ($count[$i] > 0);
    $totflips+=$count[$i];
 }
 print "Total flips = $totflips\n";
