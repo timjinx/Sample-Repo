89be0337d686fa8596766689c4cd9d4383c8ab89
Tim Jinkerson <jinkerson@outlook.com>
2018-03-12 18:33:38 +0000

    First commit of mortgage.rb
    I'm sure it's tatty, Ruby isn't a language I particularly take to

---
+++ ruby/mortgage.rb
@@ -0,0 +1,31 @@
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: mortage.rb [options]"
+
+  opts.on("-a", "--amount [AMOUNT]", "Amount borrowed") do |a| 
+     options[:amount] = a.to_i
+  end
+  opts.on("-r", "--rate [RATE]", "Interest rate") do |r| 
+     options[:rate] = r.to_f
+  end
+  opts.on("-d", "--duration [DURATION]", "Loan duration in years") do |d| 
+     options[:duration] = d.to_i
+  end
+end.parse!
+
+v_rm = options[:rate] / 12
+v_dm = options[:duration].to_i * 12
+v_amount = options[:amount].to_f
+
+top_line = v_amount * v_rm * (( 1 + (v_rm / 100))**v_dm) 
+bottom_line = 100 * (( (1 + ( v_rm / 100 ))**v_dm) -1)
+mp = top_line / bottom_line
+ap = mp * 12
+printf "The amount borrowed is %d\n", v_amount.round
+printf "The interest rate is %s\%\n", options[:rate].to_s
+printf "The loan duration is %s years\n", options[:duration].to_s
+printf "Annual repayments will be\t%8.2f\n", ap.round(2)
+printf "Monthly repayments will be\t%8.2f\n", mp.round(2)
+printf "Total cost of loan will be\t%8.2f\n", (ap * options[:duration]).round(2)
