b0c44ea22857a7e130d5196c623eab468abcb655
Tim Jinkerson <tjjinkerson@gmail.com>
2018-03-13 10:02:44 +0000

    Tidy up number formatting

--- ruby/mortgage.rb
+++ ruby/mortgage.rb
@@ -1,3 +1,4 @@
+#coding: utf-8
 require 'optparse'
 
 options = {}
@@ -23,9 +24,9 @@ top_line = v_amount * v_rm * (( 1 + (v_rm / 100))**v_dm)
 bottom_line = 100 * (( (1 + ( v_rm / 100 ))**v_dm) -1)
 mp = top_line / bottom_line
 ap = mp * 12
-printf "The amount borrowed is %d\n", v_amount.round
+printf "The amount borrowed is £%0.2f\n", v_amount.round(2)
 printf "The interest rate is %s\%\n", options[:rate].to_s
 printf "The loan duration is %s years\n", options[:duration].to_s
-printf "Annual repayments will be\t%8.2f\n", ap.round(2)
-printf "Monthly repayments will be\t%8.2f\n", mp.round(2)
-printf "Total cost of loan will be\t%8.2f\n", (ap * options[:duration]).round(2)
+printf "Annual repayments will be £%0.2f\n", ap.round(2)
+printf "Monthly repayments will be £%0.2f\n", mp.round(2)
+printf "Total cost of loan will be £%0.2f\n", (ap * options[:duration]).round(2)
