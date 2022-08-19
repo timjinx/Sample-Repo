859b0921877691369fa64022351ab4ed32887b25
Tim Jinkerson <jinkerson@outlook.com>
2022-08-19 08:32:23 +0100

    test code to read an API

---
+++ python/iss_crew.py
@@ -0,0 +1,19 @@
+import json
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+my_url = "http://api.open-notify.org/astros.json"
+def main():
+    session = requests.Session()
+    retry = Retry(connect=3, backoff_factor=0.5)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+    print("Looking for the International Space Station Crew")
+    response = session.get(my_url)
+    if response.status_code > 200 :
+       print(response)
+    else:
+       parsed = json.loads(response.text)
+       print(json.dumps(parsed, indent=4, sort_keys=True))
+
+main()
